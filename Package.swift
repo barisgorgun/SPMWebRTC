0de6744753394a09771e541b2bb9055282e72952
baris.gorgun <barisgorgunn@gmail.com>
2022-04-22 00:50:12 +0300

    sync

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WebRTC",
-            targets: ["WebRTCFramework"]),
+            targets: ["WebRTC"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "WebRTCFramework",
+        .binaryTarget(name: "WebRTC",
                       path: "WebRTC.xcframework"),
     ]
 )
